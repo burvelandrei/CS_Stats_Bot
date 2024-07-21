16f3ec2965c67dde7747b22c578e335306b87d53
Andrei Burvel <burvelandrei@gmail.com>
2024-07-21 13:14:07 +0300

    init

---
+++ lexicon/lexicon.py
@@ -0,0 +1,10 @@
+LEXICON_MENU_COMMANDS_RU: dict[str, str] = {
+    '/menu': 'Главное меню',
+    '/personal_account': 'Личный кабинет',
+    '/add_balance': 'Пополнить баланс',
+    "/share_friend": "Поделиться с другом",
+    '/referal_menu': 'Получить бонус',
+    '/input_promocode': 'Ввести промокод',
+    '/support_menu': 'Тех. поддержка',
+    '/my_id': 'My ID',
+    }
